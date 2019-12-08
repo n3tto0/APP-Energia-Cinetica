19362f50e7748f92a3fc5c9083d0021f1ab4fe1b
Shark <mauricion978@gmail.com>
2019-12-08 16:02:44 -0300

    Aplicação Energia Cinética em Python

---
+++ Energia-CInetica.py
@@ -0,0 +1,68 @@
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk, Image 
+from  functools import partial
+#-----------------------------------------------= Master =----------------------------------------
+janela = Tk()
+janela.title()
+
+largura = 375
+altura = 234
+
+largura_screen = janela.winfo_screenwidth()
+altuea_screen = janela.winfo_screenheight()
+
+posx = largura_screen/2 - largura/2
+posy = altuea_screen/2 - altura/2
+
+janela.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
+janela.resizable(False, False)
+
+#------------------------------------------------= BG =-------------------------------------------
+
+image1 = Image.open("background.png")
+image2 = ImageTk.PhotoImage(image1)
+background_label = Label(janela, image=image2)
+background_label.image1=image1
+background_label.place(x= 0, y= 0, width=375, height=234)
+
+#-------------------------------------------------------------------------------------------------
+
+#--------------------------------------------= Funções =------------------------------------------
+def Calc_Cinetica(calc):
+	m = float(ed1.get())
+	v = float(ed2.get())
+	lb["text"] = m * (v ** 2) * (1/2)
+
+#-------------------------------------------------------------------------------------------------
+
+#--------------------------------------------= Labels =-------------------------------------------
+
+ed1 = Entry(janela,
+	bd=2)
+ed1.place(x=5, y=5)
+
+ed2 = Entry(janela,
+	bd=2)
+ed2.place(x=5, y=25)
+
+lb = Label(janela,
+	text="")
+lb.place(x=65, y=50)
+
+lb2 = Label(janela,
+	text="Resuldado:")
+lb2.place(x=5, y=50)
+
+#-------------------------------------------------------------------------------------------------
+
+#--------------------------------------------= Botões =-------------------------------------------
+
+bt1 = Button(janela, text="Calcular",
+	bd=3)
+bt1["command"] = partial(Calc_Cinetica, bt1)
+bt1.place(x=130, y=10)
+
+#-------------------------------------------------------------------------------------------------
+
+janela.mainloop()
