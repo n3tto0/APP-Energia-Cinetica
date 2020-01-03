93fd724433d5c3ee0943910024d45be02796f7fb
unknown <mauricion978@gmail.com>
2020-01-03 01:40:14 -0300

    Modificação no Código

--- Energia-CInetica.py
+++ Energia-CInetica.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import *    # Por Maurício N. (Shark)
 import tkinter as tk
 from PIL import ImageTk, Image 
 from  functools import partial
@@ -20,7 +20,7 @@ janela.resizable(False, False)
 
 #------------------------------------------------= BG =-------------------------------------------
 
-image1 = Image.open("background.png")
+image1 = Image.open("background.png") # <-- uma forma criativa de ser usar uma imagem como Background!
 image2 = ImageTk.PhotoImage(image1)
 background_label = Label(janela, image=image2)
 background_label.image1=image1
@@ -29,16 +29,16 @@ background_label.place(x= 0, y= 0, width=375, height=234)
 #-------------------------------------------------------------------------------------------------
 
 #--------------------------------------------= Funções =------------------------------------------
-def Calc_Cinetica(calc):
-	m = float(ed1.get())
+def Calc_Cinetica(calc): #<-- função a ser chamada por um button!
+	m = float(ed1.get()) 
 	v = float(ed2.get())
-	lb["text"] = m * (v ** 2) * (1/2)
-
+	lb["text"] = m * (v ** 2) * (1/2) #<-- O calculo precisa ser necessariamente escrito aqui,
+									  # já que foi escrito para ser rodado com Partial
 #-------------------------------------------------------------------------------------------------
 
 #--------------------------------------------= Labels =-------------------------------------------
 
-ed1 = Entry(janela,
+ed1 = Entry(janela,    # Utilizei o place para ter uma admistração maior do layuot do programa!!
 	bd=2)
 ed1.place(x=5, y=5)
 
@@ -60,7 +60,7 @@ lb2.place(x=5, y=50)
 
 bt1 = Button(janela, text="Calcular",
 	bd=3)
-bt1["command"] = partial(Calc_Cinetica, bt1)
+bt1["command"] = partial(Calc_Cinetica, bt1) #<-- Partial sendo utilizado nessa parte do código!! 
 bt1.place(x=130, y=10)
 
 #-------------------------------------------------------------------------------------------------
